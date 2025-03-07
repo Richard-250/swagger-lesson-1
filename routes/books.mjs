fcd90931bf12cb0bf3eccf8d8498dd972d609ed3
Richard-250 <cyubahirorichard250@gmail.com>
2025-03-07 15:20:14 +0200

    Resolved all merge conflicts

--- routes/books.mjs
+++ routes/books.mjs
@@ -1,36 +1,10 @@
 import express from 'express';
-import { nanoid } from 'nanoid';
-// import { nanoid } from 'nanoid';
+// import nanoid from 'nanoid';
+
 const router = express.Router();
 
-const isLength = 8;
-
 router.get('/', (req, res) => {
-    console.log("Books to be shown:", req.app.db.data.books);
-    const books = req.app.db.data.books; // Ensure you access the correct database data
-    res.json(books);
-});
-
-router.get('/:id', (req, res) => {
-    const book = req.app.db.data.books.find({ id: req.params.id }).value();
-    res.status(201).json(book);
+    res.send('Are you there!')
 })
-
-router.post('/', async (req, res) => {
-    try {
-        const { title, author } = req.body
-        const book = {
-            id: nanoid(isLength),
-            title,
-            author
-        }
-        req.app.db.data.books.push(book)
-        res.status(201).json({ message: 'Book added successfully', book });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send('Internal Server Error');
-    }
-})
-
 
 export default router
